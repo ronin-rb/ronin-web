b3e1a7bd9f39f54bf83a425b45550d29b8c0efc3
postmodern <postmodern.mod3@gmail.com>
2009-07-23 16:06:33 -0700

    Rewrote the Hosts module to use before blocks.

--- lib/ronin/web/server/hosts.rb
+++ lib/ronin/web/server/hosts.rb
@@ -29,71 +29,34 @@ module Ronin
       module Hosts
         def self.included(base)
           base.module_eval do
-            @@vhost_patterns = {}
-            @@vhosts = {}
-
-            @@vhost_patterns_mutex = Mutex.new
-            @@vhosts_mutex = Mutex.new
-
             #
             # Registers the given _server_ to be called when receiving
             # requests to host names which match the specified _pattern_.
             #
-            #   Server.hosts_like(/^a[0-9]\./, FileProxy)
+            #   MyApp.hosts_like(/^a[0-9]\./, FileProxy)
             #
             def self.hosts_like(pattern,server)
-              @@vhost_patterns_mutex.synchronize do
-                @@vhost_patterns[pattern] = server
+              before do
+                if request.host.match(pattern)
+                  server.call(request)
+                end
               end
             end
 
             #
             # Registers the specified _server_ to be called when receiving
-            # requests with the specified host _name_.
+            # requests for the specified host _name_.
             #
-            #   Server.host('cdn.evil.com', EvilServer)
+            #   MyApp.host('cdn.evil.com', EvilServer)
             #
             def self.host(name,server)
-              @@vhosts_mutex.synchronize do
-                @@vhosts[name.to_s] = server
-              end
-            end
-
-            #
-            # Returns the server that handles requests for the specified host
-            # _name_.
-            #
-            def host_for(name)
               name = name.to_s
 
-              @@vhosts_mutex.synchronize do
-                if @@vhosts.has_key?(name)
-                  return @@vhosts[name]
+              before do
+                if request.host == name
+                  server.call(request)
                 end
               end
-
-              @@vhost_patterns_mutex.synchronize do
-                @@vhost_patterns.each do |pattern,server|
-                  return server if name.match(pattern)
-                end
-              end
-
-              return nil
-            end
-
-            #
-            # The method which receives all requests.
-            #
-            def self.call(env)
-              http_host = env['HTTP_HOST']
-
-              if http_host
-                if (server = self.host_for(http_host))
-                  return server.call(env)
-                end
-              end
-
-              return super(env)
             end
 
             protected
