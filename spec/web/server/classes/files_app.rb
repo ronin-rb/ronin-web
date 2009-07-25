e7071a3d571a4db9e09966d9be66810fb042f267
postmodern <postmodern.mod3@gmail.com>
2009-07-24 22:32:58 -0700

    Organize the code.

--- spec/web/server/classes/files_app.rb
+++ spec/web/server/classes/files_app.rb
@@ -2,10 +2,10 @@ require 'ronin/web/server/base'
 
 class FilesApp < Ronin::Web::Server::Base
 
-  file '/tests/file/missing', 'should_be_missing'
-
   file '/tests/file',
        File.join(File.dirname(__FILE__),'files','file.txt')
+
+  file '/tests/file/missing', 'should_be_missing'
 
   get '/tests/content_type/custom' do
     return_file File.join(File.dirname(__FILE__),'files','page.xml'),
