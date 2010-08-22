ad81257eb7eda8a7e7ef08f073d03f0920d0edd2
Postmodern <postmodern.mod3@gmail.com>
2010-08-21 17:39:30 -0700

    Moved middleware helper methods into Ronin::Web::Middleware::Helpers.

--- lib/ronin/web/middleware/helpers.rb
+++ lib/ronin/web/middleware/helpers.rb
@@ -19,76 +19,62 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 
-require 'ronin/web/middleware/helpers'
+require 'ronin/web/middleware/response'
+
+require 'rack'
 
 module Ronin
   module Web
     module Middleware
-      class Base
-
-        include Helpers
-
-        # The default status code to return
-        DEFAULT_STATUS = 200
-
-        # The status code to return
-        attr_accessor :default_status
-
-        # The default headers to return
-        attr_reader :default_headers
-
+      #
+      # A module containing helper methods that can be used in Rack
+      # Middleware and Rack Apps.
+      #
+      module Helpers
         #
-        # Creates a new middleware object.
+        # Unescapes the given data.
         #
-        # @param [#call] app
-        #   The application the middleware will sit in front of.
+        # @param [String] data
+        #   The given data.
         #
-        # @param [Hash] options
-        #   Additional options.
-        #
-        # @option options [Integer] :default_status (DEFAULT_STATUS)
-        #   The status code to return.
-        #
-        # @option options [Hash] :default_headers
-        #   The headers to return.
-        #
-        # @yield [middleware]
-        #   If a block is given, it will be passed the new middleware.
-        #
-        # @yieldparam [Base] middleware
-        #   The new middleware object.
+        # @return [String]
+        #   The unescaped data.
         #
         # @since 0.2.2
         #
-        def initialize(app,options={})
-          @app = app
-
-          @default_status = (options[:default_status] || DEFAULT_STATUS)
-          @default_headers = {}
-
-          if options.has_key?(:default_headers)
-            @default_headers.merge!(options[:default_headers])
-          end
-
-          yield self if block_given?
+        def unescape(data)
+          Rack::Utils.unescape(data)
         end
 
         #
-        # Passes the request to the application.
+        # Sanitizes a path received by the middleware.
         #
-        # @param [Hash, Rack::Request] env
-        #   The request.
+        # @param [String] path
+        #   The unsanitized path.
         #
-        # @return [Rack::Response]
-        #   The response.
+        # @return [String]
+        #   The unescaped and absolute path.
         #
         # @since 0.2.2
         #
-        def call(env)
-          @app.call(env)
+        def sanitize_path(path)
+          File.expand_path(unescape(path))
         end
 
-        protected
+        #
+        # Returns the MIME type for a path.
+        #
+        # @param [String] path
+        #   The path to determine the MIME type for.
+        #
+        # @return [String]
+        #   The MIME type for the path.
+        #
+        # @since 0.2.2
+        #
+        def mime_type_for(path)
+          Rack::Mime.mime_type(File.extname(path))
+        end
 
         #
         # Creates a new response.
@@ -120,12 +106,38 @@ module Ronin
         # @since 0.2.2
         #
         def response(body=[],headers={},status=nil)
-          status ||= @default_status
-          headers = @default_headers.merge(headers)
+          response = Response.new(body,status,headers)
 
-          return super(body,status,headers)
+          yield(response) if block_given?
+          return response
         end
 
+        #
+        # Creates a new response for a file.
+        #
+        # @param [String] path
+        #   The path to the file.
+        #
+        # @param [Hash] headers
+        #   Additional headers for the response.
+        #
+        # @param [Integer] status
+        #   The HTTP Status Code for the response.
+        #
+        # @return [Response]
+        #   The new response object.
+        #
+        # @see #response
+        #
+        # @since 0.2.2
+        #
+        def response_for(path,headers={},status=nil)
+          response(
+            File.new(path),
+            headers.merge('Content-Type' => mime_type_for(path)),
+            status
+          )
+        end
       end
     end
   end
