ee72579c3863d839fc5c934d9da031b84510007e
Postmodern <postmodern.mod3@gmail.com>
2010-08-30 22:52:32 -0700

    Fixed a spec.

---
+++ spec/web/middleware/proxy_spec.rb
@@ -0,0 +1,61 @@
+require 'spec_helper'
+require 'ronin/web/middleware/proxy'
+
+require 'web/helpers/rack_app'
+
+describe Web::Middleware::Proxy do
+  include Helpers::Web::RackApp
+
+  before(:all) do
+    self.app = Class.new(Sinatra::Base) do
+      use Ronin::Web::Middleware::Proxy, :path => '/login' do |proxy|
+        proxy.every_request do |request|
+          request.scheme = 'https'
+          request.host = 'github.com'
+          request.port = 443
+          request.referer = 'http://github.com/login'
+        end
+
+        proxy.every_response do |response|
+          response.body.each { |chunk| chunk.gsub!('https:','http:') }
+        end
+      end
+
+      get '/' do
+        'unproxied'
+      end
+
+      get '/login' do
+        'unproxied login'
+      end
+    end
+  end
+
+  it "should proxy requests that match the proxies filters" do
+    get '/login'
+
+    last_response.should be_ok
+    last_response.body.should_not == 'unproxied login'
+  end
+
+  it "should allow rewriting proxied requests" do
+    get '/login'
+
+    last_response.should be_ok
+    last_response.body.should include('Log in')
+  end
+
+  it "should allow rewriting proxied responses" do
+    get '/login'
+
+    last_response.should be_ok
+    last_response.body.should_not include('https:')
+  end
+
+  it "should still route un-matched requests to the app" do
+    get '/'
+
+    last_response.should be_ok
+    last_response.body.should == 'unproxied'
+  end
+end
