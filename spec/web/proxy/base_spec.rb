ac2a71dddb54e94832b85fdd552f9acaf5a0aa27
Postmodern <postmodern.mod3@gmail.com>
2012-08-23 20:01:28 -0700

    Removed an useless test.

--- spec/web/proxy/base_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'ronin/web/proxy/base'
-
-describe Web::Proxy::Base do
-  it "should run on a different port than Web::Server::Base" do
-    Web::Proxy::Base.port.should_not == Web::Server::Base.port
-  end
-end
