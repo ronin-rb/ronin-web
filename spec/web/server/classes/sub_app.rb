5c56bf954ef05a4d412a0447fdcc9aa8c7d0c004
Postmodern <postmodern.mod3@gmail.com>
2013-06-18 17:08:36 -0700

    Fixed the TestApp and SubApp.

--- spec/web/server/classes/sub_app.rb
+++ spec/web/server/classes/sub_app.rb
@@ -2,11 +2,11 @@ require 'ronin/web/server/base'
 
 class SubApp < Ronin::Web::Server::Base
 
-  get '/tests/subapp/hello' do
+  get '/hello' do
     'SubApp greets you'
   end
 
-  get '/tests/subapp/' do
+  get '/' do
     'SubApp'
   end
 
