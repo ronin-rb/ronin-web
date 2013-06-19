da3f5f904ac6634d8f92e473e176ef881d07f791
Postmodern <postmodern.mod3@gmail.com>
2013-06-18 21:47:27 -0700

    Group specs.

--- spec/web/web_spec.rb
+++ spec/web/web_spec.rb
@@ -9,75 +9,91 @@ describe Web do
     Web.const_defined?('VERSION').should == true
   end
 
-  it "should be able to parse HTML" do
-    doc = Web.html(%{
-      <html>
-        <body>Hello</body>
-      </html>
-    })
+  describe "html" do
+    it "should be able to parse HTML" do
+      doc = Web.html(%{
+        <html>
+          <body>Hello</body>
+        </html>
+      })
 
-    doc.at('body').inner_text.should == "Hello"
+      doc.at('body').inner_text.should == "Hello"
+    end
   end
 
-  it "should be able to build HTML documents" do
-    doc = Web.build_html do
-      html {
-        body {
-          div { text("hello") }
+  describe "build_html" do
+    it "should be able to build HTML documents" do
+      doc = Web.build_html do
+        html {
+          body {
+            div { text("hello") }
+          }
         }
-      }
+      end
+
+      doc.to_html.should include("<html><body><div>hello</div></body></html>")
+    end
+  end
+
+  describe "html" do
+    it "should be able to parse XML" do
+      doc = Web.html(%{
+        <?xml version="1.0"?>
+        <root>
+          <stuff>Hello</stuff>
+        </root>
+      })
+
+      doc.at('stuff').inner_text.should == "Hello"
+    end
+  end
+
+  describe "build_xml" do
+    it "should be able to build XML documents" do
+      doc = Web.build_xml do
+        root {
+          stuff(name: 'bla') { text("hello") }
+        }
+      end
+
+      doc.to_xml.should include("<root>\n  <stuff name=\"bla\">hello</stuff>\n</root>")
+    end
+  end
+
+  describe "proxy" do
+    it "should have a default proxy" do
+      Web.proxy.should_not be_nil
     end
 
-    doc.to_html.should include("<html><body><div>hello</div></body></html>")
+    it "should disable the proxy by default" do
+      Web.proxy.should_not be_enabled
+    end
   end
 
-  it "should be able to parse XML" do
-    doc = Web.html(%{
-      <?xml version="1.0"?>
-      <root>
-        <stuff>Hello</stuff>
-      </root>
-    })
-
-    doc.at('stuff').inner_text.should == "Hello"
+  describe "user_agent_aliases" do
+    it "should provide User-Agent aliases" do
+      Web.user_agent_aliases.should_not be_empty
+    end
   end
 
-  it "should be able to build XML documents" do
-    doc = Web.build_xml do
-      root {
-        stuff(name: 'bla') { text("hello") }
-      }
+  describe "#user_agent" do
+    it "should provide a default User-Agent" do
+      Web.user_agent.should be_nil
     end
 
-    doc.to_xml.should include("<root>\n  <stuff name=\"bla\">hello</stuff>\n</root>")
+    it "should allow setting of the User-Agent string using an alias" do
+      Web.user_agent_alias = 'Mac FireFox'
+
+      Web.user_agent.should == "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
+    end
   end
 
-  it "should have a default proxy" do
-    Web.proxy.should_not be_nil
-  end
+  describe "#open" do
+    it "should open URLs as temporary files" do
+      file = Web.open(url)
 
-  it "should disable the proxy by default" do
-    Web.proxy.should_not be_enabled
-  end
-
-  it "should provide User-Agent aliases" do
-    Web.user_agent_aliases.should_not be_empty
-  end
-
-  it "should provide a default User-Agent" do
-    Web.user_agent.should be_nil
-  end
-
-  it "should allow setting of the User-Agent string using an alias" do
-    Web.user_agent_alias = 'Mac FireFox'
-
-    Web.user_agent.should == "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2) Gecko/20100115 Firefox/3.6"
-  end
-
-  it "should open URLs as temporary files" do
-    file = Web.open(url)
-    
-    file.read.should include(title)
+      file.read.should include(title)
+    end
   end
 
   describe "agent" do
@@ -86,16 +102,20 @@ describe Web do
     end
   end
 
-  it "should be able to get Mechanize pages" do
-    page = Web.get(url)
+  describe "get" do
+    it "should be able to get Mechanize pages" do
+      page = Web.get(url)
 
-    page.class.should == Mechanize::Page
-    page.at('title').inner_text.should include(title)
+      page.class.should == Mechanize::Page
+      page.at('title').inner_text.should include(title)
+    end
   end
 
-  it "should be able to get the bodies of Mechanize pages" do
-    body = Web.get_body(url)
+  describe "get_body" do
+    it "should be able to get the bodies of Mechanize pages" do
+      body = Web.get_body(url)
 
-    body.should include(title)
+      body.should include(title)
+    end
   end
 end
