dbf8962ddb68ec1429e3d1f84aabd8880880a8fd
Postmodern <postmodern.mod3@gmail.com>
2022-01-26 10:21:37 -0800

    Added a placeholder for the `proxy` sub-command (issue #39).

---
+++ lib/ronin/web/cli/commands/proxy.rb
@@ -0,0 +1,41 @@
+#
+# ronin-web - A collection of useful web helper methods and commands.
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-web.
+#
+# ronin-web is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-web is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ronin-web.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/web/cli/command'
+
+module Ronin
+  module Web
+    class CLI
+      module Commands
+        class Proxy < Command
+
+          description 'Starts a web proxy'
+
+          man_page 'ronin-web-proxy.1'
+
+          def run
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ man/ronin-web-proxy.1.md
@@ -0,0 +1,22 @@
+# ronin-web-proxy 1 "2022-01-01" Ronin Web "User Manuals"
+
+## SYNOPSIS
+
+`ronin-web-proxy` [*options*]
+
+## DESCRIPTION
+
+Starts a web proxy.
+
+## ENVIRONMENT
+
+*HTTP_PROXY*
+	Sets the global HTTP proxy.
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
+## SEE ALSO
+
+ronin-web-server(1) ronin-web-spider(1) ronin-web-proxy(1) ronin-web-diff(1)
