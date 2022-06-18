0f4cfbd41b9cb04002ae218dd141e9f95bf688af
Postmodern <postmodern.mod3@gmail.com>
2022-06-17 18:44:02 -0700

    Flushed out the `server` sub-command (closes #58).

--- lib/ronin/web/cli/commands/server.rb
+++ lib/ronin/web/cli/commands/server.rb
@@ -20,18 +20,135 @@
 #
 
 require 'ronin/web/cli/command'
+require 'ronin/core/cli/logging'
+require 'ronin/web/server'
 
 module Ronin
   module Web
     class CLI
       module Commands
+        #
+        # Starts a web server.
+        #
+        # ## Usage
+        #
+        #     ronin-web server [options]
+        #
+        # ## Options
+        #
+        #    -H, --host HOST                  Host name or IP to bind to (Default: localhost)
+        #    -p, --port PORT                  Port number to listen on (Default: 8000)
+        #    -A, --basic-auth USER:PASSWORD   Sets up Basic-Authentication
+        #    -d, --dir /PATH:DIR              Mounts a directory to the given PATH
+        #    -f, --file /PATH:FILE            Mounts a file to the given PATH
+        #    -r, --root DIR                   Root directory to serve
+        #    -R, --redirect /PATH:URL         Registers a 302 Found redirect at the given PATH
+        #    -h, --help                       Print help information
+        # 
         class Server < Command
+
+          include Core::CLI::Logging
+
+          class App < Ronin::Web::Server::Base
+          end
+
+          option :host, short: '-H',
+                        value: {
+                          type:    String,
+                          usage:   'HOST',
+                          default: 'localhost'
+                        },
+                        desc: 'Host name or IP to bind to' do |host|
+                          App.host = host
+                        end
+
+          option :port, short: '-p',
+                        value: {
+                          type:    Integer,
+                          usage:   'PORT',
+                          default: App.port
+                        },
+                        desc: 'Port number to listen on' do |port|
+                          App.port = port
+                        end
+
+          option :basic_auth, short: '-A',
+                              value: {
+                                type: String,
+                                usage: 'USER:PASSWORD'
+                              },
+                              desc: 'Sets up Basic-Authentication' do |str|
+                                auth_user, auth_password = str.split(':',2)
+
+                                App.use(Rack::Auth::Basic, "Restricted Area") do |username,password|
+                                  username == auth_user && password == auth_password
+                                end
+                              end
+
+          option :dir, short: '-d',
+                       value: {
+                         type:  String,
+                         usage: '/PATH:DIR'
+                       },
+                       desc: 'Mounts a directory to the given PATH' do |str|
+                         url_path, dir = str.split(':',2)
+
+                         App.directory(url_path,dir)
+                       end
+
+          option :file, short: '-f',
+                        value: {
+                          type: String,
+                          usage: '/PATH:FILE'
+                        },
+                        desc: 'Mounts a file to the given PATH' do |str|
+                          url_path, file = str.split(':',2)
+
+                          App.file(url_path,file)
+                        end
+
+          option :root, short: '-r',
+                        value: {
+                          type: String,
+                          usage: 'DIR'
+                        },
+                        desc: 'Root directory to serve'
+
+          option :redirect, short: '-R',
+                            value: {
+                              type:  String,
+                              usage: '/PATH:URL'
+                            },
+                            desc: 'Registers a 302 Found redirect at the given PATH' do |str|
+                              route_path, url = str.split(':',2)
+
+                              App.get(route_path) do
+                                redirect url
+                              end
+                            end
 
           description 'Starts a web server'
 
           man_page 'ronin-web-server.1'
 
           def run
+            if options[:root]
+              App.public_dir = options[:root]
+            else
+              App.any('*') do
+                puts "#{request.request_method} #{request.path}"
+
+                request.headers.each do |name,value|
+                  puts "#{name}: #{value}"
+                end
+
+                puts request.body.read
+              end
+            end
+
+            log_info "Starting web server listening on #{App.host}:#{App.port} ..."
+            App.run!
+            log_info "Shutting down ..."
           end
 
         end
--- man/ronin-web-server.1.md
+++ man/ronin-web-server.1.md
@@ -8,10 +8,36 @@
 
 Starts a web server.
 
+## OPTIONS
+
+`-H`, `--host` *HOST*
+  Host name or IP to bind to. Defaults to `localhost`.
+
+`-p`, `--port` *PORT*
+  Port number to listen on. Defaults to `8000`.
+
+`-A`, `--basic-auth` *USER*:*PASSWORD*
+  Sets up Basic-Authentication with the given *USER* and *PASSWORD*.
+
+`-d`, `--dir` /*PATH*:*DIR*
+  Mounts a directory to the given *PATH*.
+
+`-f`, `--file` /*PATH*:*FILE*
+  Mounts a file to the given *PATH*.
+
+`-r`, `--root` *DIR*
+  Root directory to serve.
+
+`-R`, `--redirect` /*PATH*:*URL*
+  Registers a `302 Found` redirect at the given *PATH*
+
+`-h`, `--help`
+  Print help information
+
 ## AUTHOR
 
 Postmodern <postmodern.mod3@gmail.com>
 
 ## SEE ALSO
 
-ronin-web-server(1) ronin-web-spider(1) ronin-web-proxy(1) ronin-web-diff(1)
+ronin-web-proxy(1)
