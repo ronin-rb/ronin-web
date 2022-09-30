85a30ffe92b89fec3049f1a4364ef80cec500596
Postmodern <postmodern.mod3@gmail.com>
2022-09-29 18:14:09 -0700

    Added the `ronin-web irb` command (closes #62).

---
+++ lib/ronin/web/cli/commands/irb.rb
@@ -0,0 +1,58 @@
+#
+# ronin-web - A collection of useful web helper methods and commands.
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-web.
+#
+# ronin-web is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-web is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ronin-web.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/web/cli/command'
+require 'ronin/web/cli/ruby_shell'
+
+module Ronin
+  module Web
+    class CLI
+      module Commands
+        #
+        # Starts an interactive Ruby shell with `ronin-web` loaded.
+        #
+        # ## Usage
+        #
+        #     ronin-web irb [options]
+        #
+        # ## Options
+        #
+        #     -h, --help                       Print help information
+        #
+        class Irb < Command
+
+          description "Starts an interactive Ruby shell with ronin-web loaded"
+
+          man_page 'ronin-web-irb.1'
+
+          #
+          # Runs the `ronin-web irb` command.
+          #
+          def run
+            require 'ronin/web'
+            CLI::RubyShell.start
+          end
+
+        end
+      end
+    end
+  end
+end
---
+++ man/ronin-web-irb.1.md
@@ -0,0 +1,22 @@
+# ronin-web-irb 1 "May 2022" Ronin "User Manuals"
+
+## SYNOPSIS
+
+`ronin-web irb` [*options*]
+
+## DESCRIPTION
+
+Starts an interactive Ruby shell with `ronin/web` loaded.
+
+## OPTIONS
+
+`-h`, `--help`
+  Print help information
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
+## SEE ALSO
+
+ronin-web(1)
