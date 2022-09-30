7cb5ef2dcf5bbf7f3eb6cc8beeb0f0d88e25de98
Postmodern <postmodern.mod3@gmail.com>
2022-09-29 18:12:16 -0700

    Added `CLI::RubyShell` (issue #62).

---
+++ lib/ronin/web/cli/ruby_shell.rb
@@ -0,0 +1,52 @@
+#
+# ronin-web - A collection of useful web helper methods and commands.
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-web.
+#
+# ronin-web is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-web is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ronin-web.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/core/cli/ruby_shell'
+
+module Ronin
+  module Web
+    class CLI
+      #
+      # The interactive Ruby shell for {Ronin::Web}.
+      #
+      class RubyShell < Core::CLI::RubyShell
+
+        #
+        # Initializes the `ronin-web` Ruby shell.
+        #
+        # @param [String] name
+        #   The name of the IRB shell.
+        #
+        # @param [Object] context
+        #   Custom context to launch IRB from within.
+        #
+        # @param [Hash{Symbol => Object}] kwargs
+        #   Additional keyword arguments for
+        #   `Ronin::Core::CLI::RubyShell#initialize`.
+        #
+        def initialize(name: 'ronin-web', context: Web, **kwargs)
+          super(name: name, context: context, **kwargs)
+        end
+
+      end
+    end
+  end
+end
---
+++ spec/cli/ruby_shell_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+require 'ronin/web/cli/ruby_shell'
+
+describe Ronin::Web::CLI::RubyShell do
+  describe "#initialize" do
+    it "must default #name to 'ronin-web'" do
+      expect(subject.name).to eq('ronin-web')
+    end
+
+    it "must default #context to Ronin::Web" do
+      expect(subject.context).to be(Ronin::Web)
+    end
+  end
+end
