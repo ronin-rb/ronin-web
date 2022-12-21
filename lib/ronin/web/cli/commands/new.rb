61c93880738090453081752911dfb001cb8a07a5
Postmodern <postmodern.mod3@gmail.com>
2022-12-20 19:39:47 -0800

    Added the `ronin-web new` command (issue #64).

---
+++ lib/ronin/web/cli/commands/new.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+#
+# Copyright (c) 2006-2022 Hal Brodigan (postmodern.mod3 at gmail.com)
+#
+# This file is part of ronin-web.
+#
+# ronin-web is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ronin-web is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ronin-web.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+require 'ronin/web/cli/command'
+require 'command_kit/commands/auto_load'
+
+module Ronin
+  module Web
+    class CLI
+      module Commands
+        #
+        # Creates new projects or scripts.
+        #
+        # ## Usage
+        #
+        #     ronin-web new {nokogiri | server | spider | webapp}
+        #
+        # ## Options
+        #
+        #     -h, --help                       Print help information
+        #
+        # ## Arguments
+        #
+        #
+        #     [COMMAND]                        The command name to run
+        #     [ARGS ...]                       Additional arguments for the command
+        #
+        # ## Commands
+        #
+        #     help
+        #     nokogiri
+        #     server
+        #     spider
+        #     webapp
+        #
+        class New < Command
+
+          include CommandKit::Commands::AutoLoad.new(
+            dir:       "#{__dir__}/new",
+            namespace: "#{self}"
+          )
+
+          man_page 'ronin-web-new.1'
+
+        end
+      end
+    end
+  end
+end
---
+++ man/ronin-web-new.1.md
@@ -0,0 +1,44 @@
+# ronin-web-new 1 "May 2022" Ronin "User Manuals"
+
+## SYNOPSIS
+
+`ronin-web new` [*options*] [*COMMAND*]
+
+## DESCRIPTION
+
+Creates a new web server, spider, or nokogiri script.
+
+## ARGUMENTS
+
+*COMMAND*
+	The optional command to get detailed new information on.
+
+## OPTIONS
+
+`-h`, `--help`
+  Print help information
+
+## COMMANDS
+
+*nokogiri*
+  Generates a new `nokogiri` Ruby script.
+
+*server*
+  Generates a new `ronin-web-server` Ruby script.
+
+*spider*
+  Generates a new `ronin-web-spider` Ruby script.
+
+*webapp*
+  Generate a new `ronin-web-server` based webapp.
+
+*help*
+  Lists available `ronin new` commands.
+
+## AUTHOR
+
+Postmodern <postmodern.mod3@gmail.com>
+
+## SEE ALSO
+
+ronin-web-new-nokogiri(1) ronin-web-new-server(1) ronin-web-new-spider(1) ronin-web-new-webapp(1)
