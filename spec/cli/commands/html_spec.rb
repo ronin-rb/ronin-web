37d45da16e64bc56fe11b58bc2c1277ec06faff6
Postmodern <postmodern.mod3@gmail.com>
2023-12-05 14:54:10 -0800

    Added specs for `CLI::Commands::Html`'s option parsing logic.

--- spec/cli/commands/html_spec.rb
+++ spec/cli/commands/html_spec.rb
@@ -9,6 +9,92 @@ describe Ronin::Web::CLI::Commands::Html do
   let(:html_path)    { File.join(fixtures_dir,'page.html') }
   let(:html)         { File.read(html_path) }
 
+  describe "#option_parser" do
+    before { subject.option_parser.parse(argv) }
+
+    context "when given `--xpath XPATH`" do
+      let(:xpath) { '//p' }
+      let(:argv)  { ['--xpath', xpath] }
+
+      it "must set #query to the given XPATH expression" do
+        expect(subject.query).to eq(xpath)
+      end
+    end
+
+    context "when given `--css-path CSSPath`" do
+      let(:css_path) { 'p.class' }
+      let(:argv)     { ['--css-path', css_path] }
+
+      it "must set #query to the given CSSPath expression" do
+        expect(subject.query).to eq(css_path)
+      end
+    end
+
+    context "when given `--meta-tags`" do
+      let(:argv) { ['--meta-tags'] }
+
+      it "must set #query to '//meta'" do
+        expect(subject.query).to eq('//meta')
+      end
+    end
+
+    context "when given `--links`" do
+      let(:argv) { ['--links'] }
+
+      it "must set #query to '//a/@href'" do
+        expect(subject.query).to eq('//a/@href')
+      end
+    end
+
+    context "when given `--style`" do
+      let(:argv) { ['--style'] }
+
+      it "must set #query to '//style/text()'" do
+        expect(subject.query).to eq('//style/text()')
+      end
+    end
+
+    context "when given `--stylesheet-urls`" do
+      let(:argv) { ['--stylesheet-urls'] }
+
+      it "must set #query to '//link[@rel=\"stylesheet\"]/@href'" do
+        expect(subject.query).to eq('//link[@rel="stylesheet"]/@href')
+      end
+    end
+
+    context "when given `--javascript`" do
+      let(:argv) { ['--javascript'] }
+
+      it "must set #query to '//script/text()'" do
+        expect(subject.query).to eq('//script/text()')
+      end
+    end
+
+    context "when given `--javascript-urls`" do
+      let(:argv) { ['--javascript-urls'] }
+
+      it "must set #query to '//script/@src'" do
+        expect(subject.query).to eq('//script/@src')
+      end
+    end
+
+    context "when given `--form-urls`" do
+      let(:argv) { ['--form-urls'] }
+
+      it "must set #query to '//form/@action'" do
+        expect(subject.query).to eq('//form/@action')
+      end
+    end
+
+    context "when given `--urls`" do
+      let(:argv) { ['--urls'] }
+
+      it "must set #query to '//a/@href | //link/@href | //script/@src | //form/@action'" do
+        expect(subject.query).to eq('//a/@href | //link/@href | //script/@src | //form/@action')
+      end
+    end
+  end
+
   describe "#read" do
     context "when given a file path" do
       it "must return an opened File object" do
