7232cae994bcbb4e68e037af525d2ea581d951a8
Postmodern <postmodern.mod3@gmail.com>
2023-12-04 17:40:49 -0800

    Add the `ronin-web vulns` command (issue #81).

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'rspec'
 require 'simplecov'
+require 'webmock/rspec'
 
 SimpleCov.start
 
 RSpec.configure do |specs|
   specs.filter_run_excluding :network
+
+  specs.before(:suite) do
+    WebMock.disable_net_connect!(allow_localhost: true)
+  end
 end
