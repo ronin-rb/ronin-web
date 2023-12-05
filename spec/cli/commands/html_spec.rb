90ecc62d7b7f5731a92003e2b4b57f0a2de6f41d
Postmodern <postmodern.mod3@gmail.com>
2023-12-05 14:32:02 -0800

    Added spcs for `CLI::Commands::Html#read`.

--- spec/cli/commands/html_spec.rb
+++ spec/cli/commands/html_spec.rb
@@ -5,6 +5,43 @@ require_relative 'man_page_example'
 describe Ronin::Web::CLI::Commands::Html do
   include_examples "man_page"
 
+  let(:fixtures_dir) { File.join(__dir__,'..','..','fixtures') }
+  let(:html_path)    { File.join(fixtures_dir,'page.html') }
+  let(:html)         { File.read(html_path) }
+
+  describe "#read" do
+    context "when given a file path" do
+      it "must return an opened File object" do
+        file = subject.read(html_path)
+
+        expect(file).to be_kind_of(File)
+        expect(file.read).to eq(html)
+      end
+    end
+
+    context "when given a http:// URL" do
+      let(:url) { 'http://example.com/' }
+      let(:response_body) { html }
+
+      it "must call Ronin::Support::Network::HTTP.get_body with the URL" do
+        expect(Ronin::Support::Network::HTTP).to receive(:get_body).with(url).and_return(response_body)
+
+        expect(subject.read(url)).to be(response_body)
+      end
+    end
+
+    context "when given a https:// URL" do
+      let(:url) { 'https://example.com/' }
+      let(:response_body) { html }
+
+      it "must call Ronin::Support::Network::HTTP.get_body with the URL" do
+        expect(Ronin::Support::Network::HTTP).to receive(:get_body).with(url).and_return(response_body)
+
+        expect(subject.read(url)).to be(response_body)
+      end
+    end
+  end
+
   describe "#parse" do
     let(:html) do
       <<~HTML
