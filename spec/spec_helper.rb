7061ceb044bbe7a6e8a17646f078d5e4422161eb
Postmodern <postmodern.mod3@gmail.com>
2024-05-14 03:17:01 -0700

    Added a note about the WebMock exception for `localhost`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@ RSpec.configure do |specs|
   specs.filter_run_excluding :network
 
   specs.before(:suite) do
+    # NOTE: needed for specs that use Ronin::Web::Browser
     WebMock.disable_net_connect!(allow_localhost: true)
   end
 end
